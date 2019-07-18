52bb2ac9a9568f578107eb4b51cf50233d007602
souyouyou <im.songyangyang@gmail.com>
2019-07-18 11:01:05 +0800

    下载文件审核人员根据文件密级过滤

--- src/test/java/com/song/archives/ArchivesApplicationTests.java
+++ src/test/java/com/song/archives/ArchivesApplicationTests.java
@@ -11,25 +11,4 @@ import java.util.Date;
 
 public class ArchivesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		String tStrDN = "CN=宋洋洋,T=232324198904261835,L=jirburong,NAME=";
-		String[] dnArr = tStrDN.split(",");
-		JSONObject object = new JSONObject();
-		for (String dn:dnArr) {
-			String[] key = dn.split("=");
-			object.put(key[0],key[1]);
-		}
-
-		System.out.println(object.getString("T"));
-	}
-
-	@Test
-	public void testDate(){
-		Date overDate = DateUtil.parseStrToDate("2019-06-04", "yyyy-MM-dd", null);
-		Date sysDate = new Date();
-
-		System.out.println(overDate.after(sysDate));
-	}
-
 }
